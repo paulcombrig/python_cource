981e9f61b4d1de325eaffa20cd8a47359fffd33b
Polina <liebespakt@gmail.com>
2021-04-15 13:04:04 +0300

    update the first task, add using the function

--- iakobson_polina_dz_3/iakobson_polina_dz_3_1.py
+++ iakobson_polina_dz_3/iakobson_polina_dz_3_1.py
@@ -15,8 +15,14 @@ dictionary = {
     'ten': 'десять',
 }
 input_name = input('Введите число на латинице')
-name = input_name.strip(' ')
-if name in dictionary.keys():
-    print(f'"{dictionary[name]}" - это русский перевод с английского "{name}"')
-else:
-    print('Такого числа мы ещё не перевели')
+
+def num_translate(input_name):
+    name = input_name.strip(' ')
+    if name in dictionary.keys():
+        text = f'"{dictionary[name]}" - это русский перевод с английского "{name}"'
+        return text
+    else:
+        text = 'Такого числа мы ещё не перевели'
+        return text
+
+print(num_translate(input_name))
